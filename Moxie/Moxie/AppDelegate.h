5e159fa4d92445ce6f9452cee9e3e0c9d2af28cf
Ken <kenyu@KenYus-iMac.local>
2015-09-14 16:08:33 +0800

    Step 0

---
+++ Moxie/Moxie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Moxie
+//
+//  Created by KenYu on 9/14/15.
+//  Copyright (c) 2015 Moxtra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Moxie/Moxie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Moxie
+//
+//  Created by KenYu on 9/14/15.
+//  Copyright (c) 2015 Moxtra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
